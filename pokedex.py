e6f64e907034e900be731c64e64a5dea67c1acac
Raymone Miller <miller_raymone@hotmail.com>
2021-02-25 18:20:28 +0000

    -Update DexEntry constructor.

--- pokedex.py
+++ pokedex.py
@@ -5,8 +5,10 @@ import csv
 class Pokedex:
     """Interface for a Pokedex"""
     ###--TO DO--###
-    # make Pokedex class act like a dict object
     # make sorted views of Pokedex
+    # name, type, id, classification, height, weight, cap_rate, egg cycles,
+    # exp yield, exp growth rate, happiness, total stats, total evs,
+    # egg groups, evolution chain, owned
     # make filter methods
     
 
@@ -67,12 +69,76 @@ class Pokedex:
         """True if an entry with the specified id exists; otherwise false."""
         return True if item in self._dex_dict else False
 
+    
 class DexEntry:
     """A single entry for a Pokemon in a Pokedex."""
 
     def __init__(self, info):
-        """Constructor for a single Pokedex entry."""
+        """Constructor for a single Pokedex entry.
+        
+        :praram info: A dict containing all the information on this Pokdex entry.
+        :type info: dict
+        """
         self._dex_info = info
+        # convert data to internal list
+        self._dex_info["type"] = self._str_to_list(self._dex_info["type"])
+        self._dex_info["abilities"] = self._str_to_list(self._dex_info["abilities"])
+        self._dex_info["egg_groups"] = self._str_to_list(self._dex_info["egg_groups"])
+        self._dex_info["evolve_to"] = self._str_to_list(self._dex_info["evolve_to"])
+        self._dex_info["move_set_machine"] = self._str_to_list(self._dex_info["move_set_machine"])
+        self._dex_info["move_set_egg"] = self._str_to_list(self._dex_info["move_set_egg"])
+        self._dex_info["move_set_tutor"] = self._str_to_list(self._dex_info["move_set_tutor"])
+        # convert data to internal dicts
+        self._dex_info["name"] = self._str_to_dict(self._dex_info["name"])
+        self._dex_info["base_stats"] = self._str_to_dict(self._dex_info["base_stats"], value_type=int)
+        self._dex_info["evs"] = self._str_to_dict(self._dex_info["evs"], value_type=int)
+        self._dex_info["flavor_text"] = self._str_to_dict(self._dex_info["flavor_text"])
+        self._dex_info["move_set_level"] = self._str_to_dict(self._dex_info["move_set_level"], key_type=int)
+
+    @staticmethod
+    def _str_to_dict(string, key_type=None, value_type=None):
+        """Convert a string to a dictionary.
+
+        :param string: The string to convert.
+        :type string: str
+        :param key_type: a single argument function used to extract the
+            desired type for the keys of the dict.
+        :type key_type: function, optional
+        :param value_type: a single argument function used to extract the
+            desired type for the values of the dict.
+        :type value_type: function, optional
+
+        :return: The converted dict object.
+        :rtype: dict
+        """
+        string = string.strip("[]").split(",")
+        temp_list = [item.split(":") for item in string]
+        results = dict()
+        for key, value in temp_list:
+            if key_type:
+                key = key_type(key)
+            if value_type:
+                value = value_type(value)
+            results[key] = value
+        return results
+
+    @staticmethod
+    def _str_to_list(string, value_type=None):
+        """Convert a string to a list.
+        
+        :param string: The string to convert.
+        :type string: str
+        :param value_type: a single argument function used to extract the
+            desired type for the values of the list.
+        :type value_type: function, optional
+
+        :return: The converted list object.
+        :rtype: list
+        """
+        if value_type:
+            return [value_type(item) for item in string.strip("[]").split(",")]
+        else:
+            return string.strip("[]").split(",")
 
     @property
     def dex_info(self):
@@ -81,7 +147,7 @@ class DexEntry:
 
     @property
     def owned(self):
-        """Whether the pokemon has been seen, caught, or is unknown."""
+        """Whether the pokemon has been is unknown, seen, or owned."""
         return self._dex_info["owned"]
 
     @owned.setter
