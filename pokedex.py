a5cb5257add8420eeb2b7c6adcc9f2f23530e5b9
blackbluue <miller_raymone@hotmail.com>
2021-02-26 17:54:32 +0000

    -Further sort/filter framework

--- pokedex.py
+++ pokedex.py
@@ -1,5 +1,5 @@
-import copy
-import csv
+from copy import copy
+from csv import DictReader
 from operator import itemgetter
 
 
@@ -57,10 +57,10 @@ class Pokedex:
         try:
             # assume dex_file is a string, the path to the file
             with open(dex_file, newline="") as fh:
-                reader = csv.DictReader(fh)
+                reader = DictReader(fh)
         except TypeError:
             # dex_file is a file-like object
-            reader = csv.DictReader(dex_file)
+            reader = DictReader(dex_file)
         self._dex_dict = self._parse_info(reader)
 
     def _parse_info(self, reader):
@@ -75,7 +75,7 @@ class Pokedex:
         """
         dex_dict = dict()
         for row in reader:
-            dex_dict[row["number"]] = (DexEntry(row))
+            dex_dict[row["number"]] = DexEntry(row)
         return dex_dict
 
     def __len__(self):
@@ -104,62 +104,6 @@ class Pokedex:
     def __contains__(self, item):
         """True if an entry with the specified id exists; otherwise false."""
         return True if item in self._dex_dict else False
-
-    def sort_name(self, reverse=False):
-        """Return an iterator containing DexEntry ids from this Pokdex,
-        sorted alphabetically by the name of each pokemon.
-
-        :param reverse: A boolean value. If set to True, then the
-            elements are sorted reverse alphabetical order.
-        :type reverse: bool, optional
-        
-        :return: An iterator of this DexEntry ids.
-        :rtype: iter
-        """
-        entries = sorted(self._dex_dict.values(), key=itemgetter("name"), reverse=reverse)
-        return iter([entry["number"] for entry in entries])
-
-    def sort_type(self, reverse=False):
-        """Return an iterator containing DexEntry ids from this Pokdex,
-        sorted by the type of each pokemon.
-
-        :param reverse: A boolean value. If set to True, then the
-            elements are sorted reverse order.
-        :type reverse: bool, optional
-        
-        :return: An iterator of this DexEntry ids.
-        :rtype: iter
-        """
-        entries = sorted(self._dex_dict.values(), key=itemgetter("type"), reverse=reverse)
-        return iter([entry["number"] for entry in entries])
-
-    def sort_classification(self, reverse=False):
-        """Return an iterator containing DexEntry ids from this Pokdex,
-        sorted by the classification of each pokemon.
-
-        :param reverse: A boolean value. If set to True, then the
-            elements are sorted reverse order.
-        :type reverse: bool, optional
-        
-        :return: An iterator of this DexEntry ids.
-        :rtype: iter
-        """
-        entries = sorted(self._dex_dict.values(), key=itemgetter("classification"), reverse=reverse)
-        return iter([entry["number"] for entry in entries])
-
-    def sort_height(self, reverse=False):
-        """Return an iterator containing DexEntry ids from this Pokdex,
-        sorted by the height of each pokemon.
-
-        :param reverse: A boolean value. If set to True, then the
-            elements are sorted reverse order.
-        :type reverse: bool, optional
-        
-        :return: An iterator of this DexEntry ids.
-        :rtype: iter
-        """
-        entries = sorted(self._dex_dict.values(), key=itemgetter("height"), reverse=reverse)
-        return iter([entry["number"] for entry in entries])
    
     def sort_weight(self, reverse=False):
         """Return an iterator containing DexEntry ids from this Pokdex,
@@ -189,10 +133,87 @@ class Pokedex:
         entries = sorted(self._dex_dict.values(), key=itemgetter("capture_rate"), reverse=reverse)
         return iter([entry["number"] for entry in entries])
 
-    def sorted(self, key, reverse):
+    def sorted(self, key, reverse=False):
         """Return an iterator containing id number/DexEntry pairs for entries
-        in this Pokedex."""
-        pass
+        in this Pokedex.
+        :param key: Specifies on what criteria to sort the Pokedex.
+            Identity values are supplied as constants in this class, and can
+            be found in the documentation.
+        :param reverse: A boolean value. If set to True, then the
+            elements are sorted in reverse order.
+        :type reverse: bool, optional
+
+        :return: An iterator of this Pokdex's items.
+        :rtype: iter
+        """
+        if key == self.NAME:
+            return self._sort_simple(lambda entry: entry["name"]["English"], reverse)
+        elif key == self.TYPE:
+            return self._sort_simple(itemgetter("type"), reverse)
+        elif key == self.NUMBER:  # default sort order
+            return self._sort_simple(itemgetter("number"), reverse)
+        elif key == self.CLASSIFICATION:
+            return self._sort_simple(itemgetter("classification"), reverse)
+        elif key == self.HEIGHT:
+            return self._sort_simple(itemgetter("height"), reverse)
+        elif key == self.WEIGHT:
+            return self._sort_simple(itemgetter("weight"), reverse)
+        elif key == self.CAPTURE_RATE:
+            return self._sort_simple(itemgetter("capture_rate"), reverse)
+        elif key == self.EGG_CYCLES:
+            return self._sort_simple(itemgetter("base_egg_cycles"), reverse)
+        elif key == self.ABILITIES:
+            return self._sort_simple(itemgetter("abilities"), reverse)
+        elif key == self.EXP_YIELD:
+            return self._sort_simple(itemgetter("exp_yield"), reverse)
+        elif key == self.EXP_GROWTH_RATE:
+            return self._sort_simple(itemgetter("experience_growth"), reverse)
+        elif key == self.HAPPINESS:
+            return self._sort_simple(itemgetter("happiness"), reverse)
+        elif key == self.STATS_TOTAL:
+            sort_key = lambda entry: sum(entry["base_stats"].values())
+            return self._sort_simple(sort_key, reverse)
+        elif key == self.STATS_HP:
+            return self._sort_simple(itemgetter("stats_hp"), reverse)
+        elif key == self.STATS_ATK:
+            return self._sort_simple(itemgetter("stats_atk"), reverse)
+        elif key == self.STATS_DEF:
+            return self._sort_simple(itemgetter("stats_def"), reverse)
+        elif key == self.STATS_SP_ATK:
+            return self._sort_simple(itemgetter("stats_sp_atk"), reverse)
+        elif key == self.STATS_SP_DEF:
+            return self._sort_simple(itemgetter("stats_sp_def"), reverse)
+        elif key == self.STATS_SPD:
+            return self._sort_simple(itemgetter("stats_spd"), reverse)
+        elif key == self.EV_TOTAL:
+            sort_key = lambda entry: sum(entry["evs"].values())
+            return self._sort_simple(sort_key, reverse)
+        elif key == self.EV_HP:
+            return self._sort_simple(itemgetter("ev_hp"), reverse)
+        elif key == self.EV_ATK:
+            return self._sort_simple(itemgetter("ev_atk"), reverse)
+        elif key == self.EV_DEF:
+            return self._sort_simple(itemgetter("ev_def"), reverse)
+        elif key == self.EV_SP_ATK:
+            return self._sort_simple(itemgetter("ev_sp_atk"), reverse)
+        elif key == self.EV_SP_DEF:
+            return self._sort_simple(itemgetter("ev_sp_def"), reverse)
+        elif key == self.EV_SPD:
+            return self._sort_simple(itemgetter("ev_spd"), reverse)
+
+    def _sort_simple(key, reverse=False):
+        """Internal method for sorting Pokedex on text based fields. Supply
+        a function key to specify which field to sort on.
+        
+        :param key: Single argument function that when supplied a
+            :class:'pokedex.DexEntry' will return a value used for sorting.
+        :param reverse: A boolean value. If set to True, then the
+            elements are sorted in reverse order.
+        :type reverse: bool, optional
+        """
+        entries = sorted(self._dex_dict.values(), key=key,  reverse=reverse)
+        return iter([(value["number"], value) for value in entries])
+
 class DexEntry:
     """A single entry for a Pokemon in a Pokedex."""
 
@@ -217,6 +238,17 @@ class DexEntry:
         self._dex_info["evs"] = self._str_to_dict(self._dex_info["evs"], value_type=int)
         self._dex_info["flavor_text"] = self._str_to_dict(self._dex_info["flavor_text"])
         self._dex_info["move_set_level"] = self._str_to_dict(self._dex_info["move_set_level"], key_type=int)
+
+    @property
+    def owned(self):
+        """Whether the pokemon has been is unknown, seen, or owned."""
+        return self._dex_info["owned"]
+
+    @owned.setter
+    def owned(self, status):
+        if status.lower() not in ["unknown", "seen", "owned"]:
+            raise ValueError("Improper value for 'owned' attribute.")
+        self._dex_info["owned"] = status.lower()
 
     @staticmethod
     def _str_to_dict(string, key_type=None, value_type=None):
@@ -263,18 +295,12 @@ class DexEntry:
         else:
             return string.strip("[]").split(",")
 
-    @property
-    def dex_info(self):
-        """The collected information on this pokedex entry."""
-        return copy.deepcopy(self._dex_info)
+    def __getitem__(self, key):
+        """Return a single property from this DexEntry.
+        
+        :param key: The name of the property.
+        :type key: str
 
-    @property
-    def owned(self):
-        """Whether the pokemon has been is unknown, seen, or owned."""
-        return self._dex_info["owned"]
-
-    @owned.setter
-    def owned(self, status):
-        if status.lower() not in ["unknown", "seen", "owned"]:
-            raise ValueError("Improper value for 'owned' attribute.")
-        self._dex_info["owned"] = status.lower()
+        :return: The requested property value. Type is dependent on the property.
+        """
+        return copy(self._dex_info[key])
